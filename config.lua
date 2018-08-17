40efbb5f057a29caf1535f92d574a4ef07ac1287
x0Z3ro0x@gmail.com <x0Z3ro0x@gmail.com>
2018-08-17 02:27:28 -0700

    added
    Added a new bank to be robbed

--- config.lua
+++ config.lua
@@ -14,9 +14,16 @@ Stores = {
     -- City Locations
 	["pac_standard"] = {
 		position = { ['x'] = 254.3789, ['y'] = 226.5079, ['z'] = 101.8756 },
-		reward = math.random(50000,75000),
+		reward = math.random(75000,125000),
 		nameofstore = "Pacific Standard Bank",
-		secondsRemaining = math.random(900,1500),
+		secondsRemaining = math.random(1200,1800),
 		lastrobbed = 0
-    },
+  },
+  ["fleeca"] = {
+    position = { ['x'] = 146.9768, ['y'] = -1045.4152, ['z'] = 28.3681 },
+    reward = math.random(50000,75000),
+    nameofstore = "Fleeca Downtown Bank",
+    secondsRemaining = math.random(900,1500),
+    lastrobbed = 0
+  },
 }
